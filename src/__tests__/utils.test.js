c752c3969e09e5aedd8dd8bb1ac18557ee723da3
Boru Isako <isco.jaldy@gmail.com>
2025-04-16 20:52:46 +0300

    Complete labs

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,20 @@
 // Your tests here
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome', () => {
+  it('should return true if the word is racecar as it is a palindrome', () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  it('should return false for a word like car which is not a palindrome', () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+
+  it('should return true for palindrome with mixed case like "RaceCar"', () => {
+    expect(isPalindrome("RaceCar")).toBe(true);
+  });
+
+  it('should return false for an empty string', () => {
+    expect(isPalindrome("")).toBe(false);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,12 @@
-// Your code here
+function isPalindrome(word) {
+    if (word.length === 0) return false;
+  
+    const normalized = word.toLowerCase();
+    const reversed = normalized.split('').reverse().join('');
+    return normalized === reversed;
+  }
+  
+  module.exports = {
+    isPalindrome
+  };
+  
